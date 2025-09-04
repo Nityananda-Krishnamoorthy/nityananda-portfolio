71915aa9d424a8f34fc1a7cb17e3b4729f774949
Nityananda Krishnamoorthy <87063077+Nityananda-Krishnamoorthy@users.noreply.github.com>
2025-09-04 16:26:21 +0530

    Update SkillSection.jsx

--- src/components/SkillSection.jsx
+++ src/components/SkillSection.jsx
@@ -67,22 +67,22 @@ const SkillSection = () => {
             <BiLogoNodejs className="tech-icon cursor-pointer text-[80px] text-green-500 transition-all duration-300 hover:-translate-y-5 sm:text-[100px] md:text-[120px]" />
             <BiLogoMongodb className="tech-icon cursor-pointer text-[80px] text-green-800 transition-all duration-300 hover:-translate-y-5 sm:text-[100px] md:text-[120px]" />
             <DiPython className="tech-icon cursor-pointer text-[80px] text-yellow-400 transition-all duration-300 hover:-translate-y-5 sm:text-[100px] md:text-[120px]" />
-            <DiDjango className="tech-icon cursor-pointer text-[80px] text-green-700 transition-all duration-300 hover:-translate-y-5 sm:text-[100px] md:text-[120px]" />
+            <DiJava className="tech-icon cursor-pointer text-[80px] text-red-600 transition-all duration-300 hover:-translate-y-5 sm:text-[100px] md:text-[120px]" />
             <DiMysql className="tech-icon cursor-pointer text-[80px] text-blue-700 transition-all duration-300 hover:-translate-y-5 sm:text-[100px] md:text-[120px]" />
             <DiGit className="tech-icon cursor-pointer text-[80px] text-red-500 transition-all duration-300 hover:-translate-y-5 sm:text-[100px] md:text-[120px]" />
             <DiDocker className="tech-icon cursor-pointer text-[80px] text-blue-300 transition-all duration-300 hover:-translate-y-5 sm:text-[100px] md:text-[120px]" />
             <BiLogoGithub className="tech-icon cursor-pointer text-[80px] text-gray-400 transition-all duration-300 hover:-translate-y-5 sm:text-[100px] md:text-[120px]" />
           </div>
           {/* Row 3 - Mobile + UI/UX */}
-          <div className="flex flex-wrap justify-center gap-14">
+       {/* <div className="flex flex-wrap justify-center gap-14">
             <BiLogoAndroid className="tech-icon cursor-pointer text-[80px] text-green-600 transition-all duration-300 hover:-translate-y-5 sm:text-[100px] md:text-[120px]" />
             <BiLogoApple className="tech-icon cursor-pointer text-[80px] text-gray-200 transition-all duration-300 hover:-translate-y-5 sm:text-[100px] md:text-[120px]" />
             <SiSwift className="tech-icon cursor-pointer text-[80px] text-orange-400 transition-all duration-300 hover:-translate-y-5 sm:text-[100px] md:text-[100px]" />
             <SiKotlin className="tech-icon cursor-pointer text-[80px] text-purple-500 transition-all duration-300 hover:-translate-y-5 sm:text-[100px] md:text-[100px]" />
-            <DiJava className="tech-icon cursor-pointer text-[80px] text-red-600 transition-all duration-300 hover:-translate-y-5 sm:text-[100px] md:text-[120px]" />
+            <DiDjango className="tech-icon cursor-pointer text-[80px] text-green-700 transition-all duration-300 hover:-translate-y-5 sm:text-[100px] md:text-[120px]" />
             <BiLogoFigma className="tech-icon cursor-pointer text-[80px] text-pink-500 transition-all duration-300 hover:-translate-y-5 sm:text-[100px] md:text-[120px]" />
             <SiXcode className="tech-icon cursor-pointer text-[80px] text-blue-500 transition-all duration-300 hover:-translate-y-5 sm:text-[100px] md:text-[120px]" />
-          </div>
+          </div> */}
         </motion.div>
       </div>
     </section>
